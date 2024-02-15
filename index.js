4522e0c0c3bbd0becfb222eafe0f52670f3094c3
mahedi066 <imam25349@gmail.com>
2024-02-15 11:38:44 +0600

    all is done you can play now

--- index.js
+++ index.js
@@ -8,11 +8,17 @@ function removeClassById(remove){
     const removeClass=document.getElementById(remove);
     removeClass.classList.remove('hidden');
 }
+function removeBg(elementid){
+    const element=document.getElementById(elementid);
+    const bg= element.classList.remove('bg-[#FFA500]');
+    return bg;
+}
 function addBgColor(elementId){
     const element=document.getElementById(elementId);
    const bg= element.classList.add('bg-[#FFA500]');
    return bg;
 }
+
 function randomAlphabat(){
     const alphabatString='abcdefghijklmnopqrstuvwxyz';
     const alphabat=alphabatString.split('');
@@ -22,6 +28,16 @@ function randomAlphabat(){
     currentAlphabat.innerText=alphabats;
    addBgColor( alphabats);
 }
+function setTextElementById(elementid,value){
+    const element=document.getElementById(elementid);
+    element.innerText=value;
+}
+
+function getElementTextById(elementid){
+    const element=document.getElementById(elementid);
+    const text=element.innerText;
+    return text;
+}
 
 // utlitis
 
@@ -30,3 +46,62 @@ function play(){
     removeClassById('playground')
     randomAlphabat()
 }
+function playAgain(){
+    addClassById('final-score');
+    removeClassById('playground');
+    randomAlphabat();
+    setTextElementById('life',5)
+    setTextElementById('score',0)
+
+}
+function gameOver(){
+    addClassById('playground');
+    removeClassById('final-score');
+    const thescore=document.getElementById('score');
+
+   const gamescor=thescore.innerText
+   setTextElementById('youScore',gamescor);
+   const currentalphabat= getElementTextById('current-alphabat');
+   removeBg(currentalphabat);
+  
+}
+
+function handleKeyBordButtonPress(even){
+    const playerPresed=even.key;
+   if(playerPresed === 'Escaped'){
+    gameOver()
+   }
+    const currentAlphabat=document.getElementById('current-alphabat');
+    const curentAlphaElement=currentAlphabat.innerText;
+    const expectedAlphabat=curentAlphaElement.toLocaleLowerCase();
+
+    if(playerPresed === expectedAlphabat){
+        removeBg(expectedAlphabat);
+        randomAlphabat();
+
+        // get score 
+        const scoreElement=document.getElementById('score');
+        const currntScore=parseInt(scoreElement.innerText);
+        console.log(currntScore);
+
+        // update score 
+        const newScore=currntScore+1;
+        scoreElement.innerText=newScore;
+    }
+    else{
+        removeBg(expectedAlphabat);
+        randomAlphabat();
+
+        // get score 
+        const lifeElement=document.getElementById('life');
+        const currnlife=parseInt(lifeElement.innerText);
+
+        // update score 
+        const newlife=currnlife-1;
+        lifeElement.innerText=newlife;
+        if(newlife===0){
+            gameOver()
+        }
+    }
+}
+document.addEventListener('keyup',handleKeyBordButtonPress)
